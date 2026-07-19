b5f8e4cbf1adb2756d925a3ff88d6debf5f1aa31
Kamil Abzalov <mail@kamil-abzalov.com>
2026-07-19 20:03:36 +0300

    task-8: Add translations, add new routes in admin feature

--- src/app/features/admin/admin.routes.ts
+++ src/app/features/admin/admin.routes.ts
@@ -19,12 +19,24 @@ export const routes: Routes = [
         component: Products,
       },
       {
+        path: 'products/:id',
+        loadComponent: () => import('./pages/product/product').then(c => c.ProductDetails),
+      },
+      {
         path: 'carts',
         loadComponent: () => import('./pages/carts/carts').then(c => c.Carts),
       },
       {
+        path: 'carts/:id',
+        loadComponent: () => import('./pages/cart/cart').then(c => c.CartDetails),
+      },
+      {
         path: 'users',
         loadComponent: () => import('./pages/users/users').then(c => c.Users),
+      },
+      {
+        path: 'users/:id',
+        loadComponent: () => import('./pages/user/user').then(c => c.UserDetails),
       },
     ],
   },
